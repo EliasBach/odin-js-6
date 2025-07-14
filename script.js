99ce1d7b125860ba475414451bde161a4cdba221
EliasBach <el.bach1999@gmail.com>
2025-07-14 13:25:47 +0200

    complete exA: fibonacci

---
+++ script.js
@@ -0,0 +1,49 @@
+function fibs(n) {
+    if (n == 0) {return []} 
+    if (n == 1) {return [0]} 
+    if (n == 2) {return [0, 1]} 
+
+    const fibs_array = [0, 1]
+    let prev1 = fibs_array[1], prev2 = fibs_array[0]
+
+    for (let i = 1; i < n-1 ; i++) {
+        fibs_array.push(prev1 + prev2)
+
+        const tmp = prev1;
+        prev1 += prev2;
+        prev2 = tmp;
+    }
+    return fibs_array
+}
+
+
+function fibsRec(n) {
+    const fibN = (n) => {
+        return n <= 1 ? n : fibN(n - 1) + fibN(n - 2);
+    } 
+
+    const fibArr = []
+    for (let j = 0; j < n ; j++) {
+        fibArr.push(fibN(j))
+    }
+    return fibArr
+}
+
+// script
+console.log("for-loop")
+console.log(fibs(0))
+console.log(fibs(1))
+console.log(fibs(2))
+console.log(fibs(3))
+console.log(fibs(4))
+console.log(fibs(5))
+console.log(fibs(10))
+
+console.log("recursive")
+console.log(fibsRec(0))
+console.log(fibsRec(1))
+console.log(fibsRec(2))
+console.log(fibsRec(3))
+console.log(fibsRec(4))
+console.log(fibsRec(5))
+console.log(fibsRec(10))
